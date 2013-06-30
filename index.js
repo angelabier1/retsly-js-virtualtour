9739029f7d582bf815b4214fcab1325e3ba07f44
Kyle Campbell <mail@slajax.com>
2013-06-29 22:49:39 -0700

    WIP

--- index.js
+++ index.js
@@ -7,29 +7,37 @@ if(typeof Retsly !== 'undefined') {
 
   Retsly.Views.VirtualTour = module.exports = exports = (function(){
 
-    var Component = function(options) {
+    var Component = {};
+    Component.Basic = Backbone.View.extend({
+      initialize: function(options) {
 
-      if(!options || typeof options.mls_id === "undefined")
-        throw new Error('You must provide a mls_id: `{mls_id: mls.id}`');
+        if(!options || typeof options.mls_id === "undefined")
+          throw new Error('Retsly.Views.VirtualTour requires a mls_id: `{mls_id: mls.id}`');
 
-      if(!options || typeof options.listing_id === "undefined")
-        throw new Error('You must provide a listing_id: `{listing_id: listing.id}`');
+        if(!options || typeof options.listing_id === "undefined")
+          throw new Error('Retsly.Views.VirtualTour requires a listing_id: `{listing_id: listing.id}`');
 
-      this.options = _.extend({
-        mls_id: null,
-        listing_id: null
-      }, options);
+        if(typeof options == "undefined" || !options.target)
+          throw new Error('Retsly.Views.VirtualTour is a subview and must have a target: `{target:this}`');
 
-      this.render = function(listing) {
-        console.log('virtual tour', listing);
-      };
+        this.options = _.extend({ mls_id: null, listing_id: null }, options);
+        options.target = (typeof options.target.$el !== "undefined") ? options.target.$el : $(options.target)
 
-      new Retsly.Models.Listing({ _id: this.options.listing_id }, {
-        mls_id: this.options.mls_id,
-        callback: this.render
-      }).fetch();
+        $(options.target).append(this.$el);
 
-    };
+        var self = this;
+        new Retsly.Models.Listing({ _id: this.options.listing_id }, {
+          mls_id: this.options.mls_id,
+          callback: function(listing) { self.render(listing); }
+        }).fetch();
+
+      },
+      render: function(listing) {
+        var html = require('./templates/template');
+        var template = _.template(html);
+        this.$el.html( template( listing.toJSON() ));
+      }
+    })
 
     return Component;
 
