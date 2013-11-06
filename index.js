dd56195a54967feb3c312e3f0de43885eaf1cc58
Kyle Campbell <mail@slajax.com>
2013-11-05 18:09:29 -0800

    make component work again

--- index.js
+++ index.js
@@ -4,55 +4,47 @@
  * TODO: Need to be able to pass in a listing model
  */
 
-if(typeof Retsly !== 'undefined') {
+module.exports = exports = function(retsly){
 
-  Retsly.Views.VirtualTour = module.exports = exports = (function(){
+  var PhotoTile = require('retsly-js-phototile')(retsly)
+    , ListingInfo = require('retsly-js-listinginfo');
 
-    var PhotoTile = require('retsly-js-phototile')
-      , ListingInfo = require('retsly-js-listinginfo');
+  var Component = {};
+  Component.Basic = Backbone.View.extend({
+    index: 0,
+    className: 'retsly-component retsly-js-virtualtour span12 row-fluid',
+    initialize: function(options) {
 
-    var Component = {};
-    Component.Basic = Backbone.View.extend({
-      index: 0,
-      className: 'retsly-component retsly-js-virtualtour span12 row-fluid',
-      initialize: function(options) {
+      if(!options || typeof options.mls_id === "undefined")
+        throw new Error('Retsly.Views.VirtualTour requires a mls_id: `{mls_id: mls.id}`');
 
-        if(!options || typeof options.mls_id === "undefined")
-          throw new Error('Retsly.Views.VirtualTour requires a mls_id: `{mls_id: mls.id}`');
+      if(!options || typeof options.listing_id === "undefined")
+        throw new Error('Retsly.Views.VirtualTour requires a listing_id: `{listing_id: listing.id}`');
 
-        if(!options || typeof options.listing_id === "undefined")
-          throw new Error('Retsly.Views.VirtualTour requires a listing_id: `{listing_id: listing.id}`');
+      if(typeof options == "undefined" || !options.target)
+        throw new Error('Retsly.Views.VirtualTour is a subview and must have a target: `{target:this}`');
 
-        if(typeof options == "undefined" || !options.target)
-          throw new Error('Retsly.Views.VirtualTour is a subview and must have a target: `{target:this}`');
+      this.options = _.extend({ mls_id: null, listing_id: null }, options);
+      options.target = (typeof options.target.$el !== "undefined") ? options.target.$el : $(options.target)
 
-        this.options = _.extend({ mls_id: null, listing_id: null }, options);
-        options.target = (typeof options.target.$el !== "undefined") ? options.target.$el : $(options.target)
+      $(options.target).append(this.$el);
 
-        $(options.target).append(this.$el);
+      new PhotoTile.Basic({
+        target: this.$el,
+        mls_id: this.options.mls_id,
+        listing_id: this.options.listing_id,
+        height: 200
+      });
 
-        new PhotoTile.Basic({
-          target: this.$el,
-          mls_id: this.options.mls_id,
-          listing_id: this.options.listing_id,
-          height: 200
-        });
+      new ListingInfo.Basic({
+        target: this.$el,
+        mls_id: this.options.mls_id,
+        listing_id: this.options.listing_id,
+      });
 
-        new ListingInfo.Basic({
-          target: this.$el,
-          mls_id: this.options.mls_id,
-          listing_id: this.options.listing_id,
-        });
+    }
+  });
 
-      }
-    });
+  return Component;
 
-    return Component;
-
-  })();
-
-} else {
-  module.exports = exports = function(){
-    return; // NOOP.
-  };
 }
