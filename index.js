3cc6de3531c62ff5a30262a69c83f4f5fb7858e7
Luke Vivier <luke@vivier.ca>
2013-11-25 18:07:50 -0800

    add deps; mls_id -> vendor_id; refactor

--- index.js
+++ index.js
@@ -1,50 +1,53 @@
+
+/**
+ * Dependencies
+ */
+var ListingInfo = require('retsly-js-listinginfo');
+var PhotoTile = require('retsly-js-phototile');
+
+var Backbone = require('backbone');
+var _ = require('underscore');
+var $ = require('jquery');
+
+
 /*
  * Retsly VirtualTour Component
  * Requires Retsly SDK (Full hosted SDK including _, $, BB)
  * TODO: Need to be able to pass in a listing model
  */
+var Components = module.exports = {};
 
-module.exports = exports = function(retsly){
+Components.Basic = Backbone.View.extend({
+  index: 0,
+  className: 'retsly-component retsly-js-virtualtour span12 row-fluid',
+  initialize: function(options) {
 
-  var PhotoTile = require('retsly-js-phototile')(retsly)
-    , ListingInfo = require('retsly-js-listinginfo');
+    if(!options || typeof options.vendor_id === "undefined")
+      throw new Error('Retsly.Views.VirtualTour requires a vendor_id: `{vendor_id: \'id\'}`');
 
-  var Component = {};
-  Component.Basic = Backbone.View.extend({
-    index: 0,
-    className: 'retsly-component retsly-js-virtualtour span12 row-fluid',
-    initialize: function(options) {
+    if(!options || typeof options.listing_id === "undefined")
+      throw new Error('Retsly.Views.VirtualTour requires a listing_id: `{listing_id: listing.id}`');
 
-      if(!options || typeof options.mls_id === "undefined")
-        throw new Error('Retsly.Views.VirtualTour requires a mls_id: `{mls_id: mls.id}`');
+    if(typeof options == "undefined" || !options.target)
+      throw new Error('Retsly.Views.VirtualTour is a subview and must have a target: `{target:this}`');
 
-      if(!options || typeof options.listing_id === "undefined")
-        throw new Error('Retsly.Views.VirtualTour requires a listing_id: `{listing_id: listing.id}`');
+    this.options = _.extend({ vendor_id: null, listing_id: null }, options);
+    options.target = (typeof options.target.$el !== "undefined") ? options.target.$el : $(options.target);
 
-      if(typeof options == "undefined" || !options.target)
-        throw new Error('Retsly.Views.VirtualTour is a subview and must have a target: `{target:this}`');
+    $(options.target).append(this.$el);
 
-      this.options = _.extend({ mls_id: null, listing_id: null }, options);
-      options.target = (typeof options.target.$el !== "undefined") ? options.target.$el : $(options.target)
+    new PhotoTile.Basic({
+      target: this.$el,
+      vendor_id: this.options.vendor_id,
+      listing_id: this.options.listing_id,
+      height: 200
+    });
 
-      $(options.target).append(this.$el);
+    new ListingInfo.Basic({
+      target: this.$el,
+      vendor_id: this.options.vendor_id,
+      listing_id: this.options.listing_id,
+    });
 
-      new PhotoTile.Basic({
-        target: this.$el,
-        mls_id: this.options.mls_id,
-        listing_id: this.options.listing_id,
-        height: 200
-      });
-
-      new ListingInfo.Basic({
-        target: this.$el,
-        mls_id: this.options.mls_id,
-        listing_id: this.options.listing_id,
-      });
-
-    }
-  });
-
-  return Component;
-
-}
+  }
+});
