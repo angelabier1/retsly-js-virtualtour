067c90311b5d4258ffa50f0fa97a817b1b27e940
Kyle Campbell <mail@slajax.com>
2014-02-25 12:30:48 -0800

    added worker integration

--- Makefile
+++ Makefile
@@ -1,11 +1,18 @@
 
-build: components index.js
+build: clean components
 	@component build --dev
 
 components: component.json
 	@component install --dev
 
+dist: component.json
+	component install
+	component build
+
+test: build
+	@mocha-phantomjs test/test.html
+
 clean:
-	rm -fr build components template.js
+	rm -fr build components
 
 .PHONY: clean
---
+++ test/index.js
@@ -0,0 +1,35 @@
+
+/**
+ * Dependencies
+ */
+var assert = require('assert');
+var Retsly = require('retsly-backbone');
+var Tour = require('retsly-virtualtour');
+
+/**
+ * Tests
+ */
+
+suite('VirtualTour')
+test('is an object property', function() {
+  assert('object' == typeof Tour, 'is an object')
+});
+
+suite('VirtualTour.Basic');
+test('is a component function', function() {
+  assert('function' == typeof Tour.Basic, 'has a component')
+});
+
+test('cannot be instantiated without Retsly.create()', function() {
+  assert.throws(function() {
+    new Tour.Basic({ client_id: 'xxx', vendorID: 'sandicor', listingID: 'xxx', target: { $el: '' } });
+  }, Error);
+});
+
+test('can be instantiated after Retsly.create()', function(){
+  assert.doesNotThrow(function() {
+    Retsly.create('xxx');
+    new Tour.Basic({ client_id: 'xxx', vendorID: 'sandicor', listingID: 'xxx', target: { $el: '' } });
+  })
+});
+
